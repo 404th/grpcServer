082f301cfc3574ff528c70e522735b6cab7db961
404th <umarov.doniyor.2002@gmail.com>
2022-03-08 04:28:12 +0500

    main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
+	"time"
 
 	"github.com/404th/grpcserver/client"
+	pb "github.com/404th/grpcserver/generated/user_service"
 	"github.com/404th/grpcserver/server"
 )
 
@@ -24,4 +27,30 @@ func main() {
 	}
 
 	// runnning interfaces
+
+	//// context
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	users := make(map[string]int32)
+
+	users["Jojo"] = 75
+	users["Samuel"] = 54
+	users["Inna"] = 19
+
+	for name, age := range users {
+		us, err := client.CreateUser(ctx, &pb.NewUser{
+			Name: name,
+			Age:  age,
+		})
+		if err != nil {
+			log.Fatalf("Error while creating user: %e", err)
+		}
+
+		log.Printf(`Details:
+						Name: %s,
+						Age: %d,
+						ID: %d,		
+		`, us.GetName(), us.GetAge(), us.GetId())
+	}
 }
